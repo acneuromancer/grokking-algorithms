a1c15bf52c450b45dc33494d0cd7098b7840e067
acneuromancer <dokukodo108@gmail.com>
2019-03-11 16:41:24 +0100

    Added examples of recurcion.

---
+++ chapter_3_recursion/call_stack.py
@@ -0,0 +1,19 @@
+def greet(name):
+    print("Hello, " + name + "!")
+    
+    greet2(name)
+
+    print("Getting ready to say bye...")
+
+    bye()
+
+
+def greet2(name):
+    print("How are you, " + name + "?")
+
+
+def bye():
+    print("Ok bye!")
+
+
+greet("John Doe")
