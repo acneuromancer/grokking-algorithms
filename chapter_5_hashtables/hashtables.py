dfcb7906fb3b69babef3aa6fdb4e25777b5741d7
acneuromancer <dokukodo108@gmail.com>
2019-03-12 23:05:49 +0100

    Added a dictionary example.

---
+++ chapter_5_hashtables/hashtables.py
@@ -0,0 +1,14 @@
+prices = {
+    'apple': 8.5,
+    'banana': 10.3,
+    'lemon': 9.0,
+    'blueberries': 12.5,
+    'watermelon': 10.3
+}
+
+print(prices)
+print(prices['banana'])
+print()
+
+for key in prices:
+    print(key, prices[key])
